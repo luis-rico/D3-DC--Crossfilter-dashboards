4a4c1142f9de4ccec444167d049653265ec3bfe3
luis-rico <luis.rico70@gmail.com>
2015-09-10 13:12:06 -0700

    First two parts for GitHub
    Baseline

---
+++ Tutorial/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask import Response, jsonify
+import json
+import collections
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Sever running OK! This is to follow tutorial in http://www.codeproject.com/Articles/693841/Making-Dashboards-with-Dc-js-Part-1-Using-Crossfil"
+
+@app.route('/crossfilter')
+def crossfilter():
+    return render_template("part1.html")
+	
+@app.route('/crossfilter2')
+def crossfilter2():
+    return render_template("part2.html")
+
+
+if __name__ == "__main__":
+    app.run()
